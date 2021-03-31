5f5a77943cf118939bad367256f0f0115ecd8ab2
adnanshaikh2000 <adnanamjadshaikh@gmail.com>
2021-03-31 09:46:40 +0530

    made product page

---
+++ shopping/static/JS/popup.js
@@ -0,0 +1,13 @@
+overlay = document.getElementById("overlay");
+function on() {
+  document.getElementById("overlay").style.display = "flex";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == overlay) {
+    off();
+  }
+};
